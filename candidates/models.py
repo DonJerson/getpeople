37fe7839ed389cfffeebd554a7d1aa8c95de572c
jerson <jersonamendez@gmail.com>
2016-06-21 23:51:08 -0400

    a

--- candidates/models.py
+++ candidates/models.py
@@ -4,7 +4,6 @@ import uuid
 from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
 
-@python_2_unicode_compatible
 class Position(models.Model):
 	name = models.CharField(max_length=200)
 	salary_anual = models.IntegerField()
